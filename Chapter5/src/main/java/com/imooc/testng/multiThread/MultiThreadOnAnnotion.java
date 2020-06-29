afd50cc140e37408a71a95bfbb568bba343c8ca9
xTestLi <741297229@qq.com>
2020-06-29 12:08:57 +0800

    0629测试代码1

--- Chapter5/src/main/java/com/imooc/testng/multiThread/MultiThreadOnAnnotion.java
+++ Chapter5/src/main/java/com/imooc/testng/multiThread/MultiThreadOnAnnotion.java
@@ -3,6 +3,10 @@ package com.imooc.testng.multiThread;
 import org.testng.annotations.Test;
 
 public class MultiThreadOnAnnotion {
+    /**
+     * 多线程测试，没有关联的用例可以使用多线程，减少执行时间
+     * invocationCount表示执行次数，threadPoolSize表示线程池大小
+     */
     @Test(invocationCount = 10,threadPoolSize = 4)
     public void test(){
         System.out.println(1);
