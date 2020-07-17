5bc1c5a5e9a9948db16b9f2fcd49170d16702d12
xTestLi <741297229@qq.com>
2020-07-17 16:25:30 +0800

    0717

--- Chapter10/src/main/java/com/course/server/MyPostMethod.java
+++ Chapter10/src/main/java/com/course/server/MyPostMethod.java
@@ -1,14 +1,13 @@
 package com.course.server;
 
+import com.course.beaan.User;
 import com.sun.deploy.net.HttpResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -26,11 +25,28 @@ public class MyPostMethod {
                             @RequestParam(value = "password", required = true) String password) {
         if (username.equals("wanger") && password.equals("123456")) {
             cookie = new Cookie("login", "false");
-            cookie1 = new Cookie("login", "true");
+//            cookie1 = new Cookie("login", "true");
             response.addCookie(cookie);
-            response.addCookie(cookie1);
+//            response.addCookie(cookie1);
             return "登录访问cookie成功";
         } else
             return "你的用户名或密码错误";
+    }
+    @RequestMapping(value = "/get_list",method = RequestMethod.POST)
+    @ApiOperation(value = "获取user列表数据",httpMethod = "POST")
+    public String getUserList(HttpServletRequest request){
+        User user = new User();
+        //获取cookies
+        Cookie[] cookie = request.getCookies();
+        for(Cookie c:cookie){
+            //验证cookie是否合法
+            if(c.getName().equals("login")&&c.getValue().equals("false")){
+                user.setName("赵鹏");
+                user.setAge("18");
+                user.setSex("man");
+                return user.toString();
+            }
+        }
+        return "参数不合法";
     }
 }
