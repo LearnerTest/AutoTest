2b9e796b6ccd4e0c89dd2934a0286c1f68e6b0a7
xTestLi <741297229@qq.com>
2020-07-31 13:18:44 +0800

    0731

---
+++ Chapter12/src/test/java/com/course/test2.java
@@ -0,0 +1,29 @@
+package com.course;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class test2 {
+
+    public static void main(String[] args) {
+        String resultStr = "{\"status\": 0,\"message\": \"query ok\"," +
+                "\"result\": {\"address\": \"北京市海淀区镜桥\"," +
+                "\"address_component\": {\"nation\": \"中国\",\"province\": \"北京市\"," +
+                "\"city\": \"北京市\",\"district\": \"海淀区\"," +
+                "\"street\": \"镜桥\",\"street_number\": \"镜桥\"}}}";
+
+        JsonParser jp = new JsonParser();
+        //将json字符串转化成json对象
+        JsonObject jo = jp.parse(resultStr).getAsJsonObject();
+        //获取message对应的值
+        String message = jo.get("message").getAsString();
+        System.out.println("message：" + message);
+        //获取address对应的值
+        String address = jo.get("result").getAsJsonObject().get("address").getAsString();
+        System.out.println("address：" + address);
+        //获取city对应的值
+        String nation = jo.get("result").getAsJsonObject().get("address_component")
+                .getAsJsonObject().get("nation").getAsString();
+        System.out.println("nation：" + nation);
+    }
+}
